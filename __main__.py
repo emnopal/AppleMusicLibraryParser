f5cc6dc78cc6f3a080faa722a0b3c9dbf5358b9f
Muhammad Naufal <mnaufal0999@gmail.com>
2023-02-16 12:11:39 +0700

    update code

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
 import argparse
-from apple_music_parse_xml import AppleMusicParseXML
+import sys
 from typing import Dict, TypeVar, Optional, List
 from pathlib import Path
+from src.apple_music_parse_xml import AppleMusicParseXML
+from src.exceptions import PythonVersionIncompatible
 
 
 V = TypeVar("V")
@@ -11,10 +13,15 @@ parser: argparse.ArgumentParser = argparse.ArgumentParser()
 
 if __name__ == '__main__':
 
+    if sys.version_info.major < 3:
+        raise PythonVersionIncompatible(f"Not compatible Python version, need Python 3 but you have version {sys.version_info.major}")
+    if sys.version_info.minor < 10:
+        raise PythonVersionIncompatible(f"Not compatible Python version, need Python 3.10 or above but you have version 3.{sys.version_info.minor}")
+
     home: str = str(Path.home())
 
     parser.add_argument('-p', '--path', help="Apple Music XML file location", default=f'{home}/Library.xml')
-    parser.add_argument('-f', '--format', help="Converted xml file format; Options: (csv, json, yaml)", default='json')
+    parser.add_argument('-f', '--format', help="Converted xml file format; Options: (csv, json, yaml)", default='csv')
     parser.add_argument('-s', '--select', help="Object which want to select in XML; Separate by comma", default=None)
     parser.add_argument(
         '-i', '--index',
@@ -34,12 +41,22 @@ if __name__ == '__main__':
         obj = selected_obj.split(",")
 
     try:
-        if args.get('format') == 'csv':
-            xml_parser.save_to_csv(f"{output_path}.csv", obj, show_index=args.get('index'))
-        elif args.get('format') == 'json':
-            xml_parser.save_to_json(f"{output_path}.json", obj)
-        elif args.get('format') == 'yaml':
-            xml_parser.save_to_yaml(f"{output_path}.yaml", obj)
+        match args.get('format'):
+            case 'csv':
+                xml_parser.save_to_csv(f"{output_path}.csv", obj, show_index=args.get('index'))
+                print(f"Saved to: {output_path}.csv")
+            case 'json':
+                xml_parser.save_to_json(f"{output_path}.json", obj)
+                print(f"Saved to: {output_path}.json")
+            case 'yaml':
+                xml_parser.save_to_yaml(f"{output_path}.yaml", obj)
+                print(f"Saved to: {output_path}.yaml")
+            case 'txt':
+                xml_parser.save_to_csv(f"{output_path}.txt", obj, show_index=args.get('index'))
+                print(f"Saved to: {output_path}.txt")
+            case _:
+                xml_parser.save_to_csv(f"{output_path}.csv", obj, show_index=args.get('index'))
+                print(f"Saved to: {output_path}.csv")
     except (TypeError, KeyError):
         print("Invalid Format!")
     finally:
