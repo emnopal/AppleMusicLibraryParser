77c83b865adde2f9d09bb10f622fff1393869a9b
Muhammad Naufal <mnaufal0999@gmail.com>
2023-03-15 15:26:09 +0700

    reformating the code to follow python standards

--- src/apple_music_parse_xml.py
+++ src/apple_music_parse_xml.py
@@ -14,7 +14,6 @@ T = TypeVar("T")
 
 
 class AppleMusicParseXML:
-
     def __init__(self, path: str) -> None:
         self.path: str = path
 
@@ -26,10 +25,10 @@ class AppleMusicParseXML:
 
     def _parse_apple_music_xml(self) -> List[Element]:
         root: Element = self._read_apple_music_xml()
-        global_dict: List[Element] = root.findall('dict')
+        global_dict: List[Element] = root.findall("dict")
         global_dict_element: Element = global_dict[0]
-        tracks_dict: Element = [element for element in global_dict_element if element.tag == 'dict'][0]
-        tracklist: List[Element] = list(tracks_dict.findall('dict'))
+        tracks_dict: Element = [element for element in global_dict_element if element.tag == "dict"][0]
+        tracklist: List[Element] = list(tracks_dict.findall("dict"))
 
         return tracklist
 
@@ -40,7 +39,7 @@ class AppleMusicParseXML:
         for track in tracklist:
             track_elements: List[Element] = list(track)
             for element_num in range(len(track_elements)):
-                if track_elements[element_num].text == "Apple Music" and track_elements[element_num + 1].tag == 'true':
+                if track_elements[element_num].text == "Apple Music" and track_elements[element_num + 1].tag == "true":
                     tracks.append(track_elements)
 
         return tracks
@@ -61,11 +60,10 @@ class AppleMusicParseXML:
         for track in range(len(tracks)):
             for track_element in range(len(tracks[track])):
                 if tracks[track][track_element].tag == "key":
-                    if tracks[track][track_element].text == 'Track ID':
+                    if tracks[track][track_element].text == "Track ID":
                         track_id = tracks[track][track_element + 1].text
                     readable_track_element = tracks[track][track_element].text
-                    readable_tracks_element_temp[track_id][readable_track_element] = tracks[track][
-                        track_element + 1].text
+                    readable_tracks_element_temp[track_id][readable_track_element] = tracks[track][track_element + 1].text
                 readable_tracks_column_temp.add(readable_track_element)
 
         readable_tracks_element: Dict[str, Dict[str, str]] = dict(readable_tracks_element_temp)
@@ -86,35 +84,21 @@ class AppleMusicParseXML:
 
         return column
 
-    def save_to_json(
-            self,
-            path_to_save: Optional[str] = None,
-            cols_to_select: Optional[List[str]] = None,
-            **kwargs
-    ) -> None:
-        _path_to_save: Optional[str] = 'Library.json' if not path_to_save else path_to_save
+    def save_to_json(self, path_to_save: Optional[str] = None, cols_to_select: Optional[List[str]] = None, **kwargs) -> None:
+        _path_to_save: Optional[str] = "Library.json" if not path_to_save else path_to_save
         readable_tracks_element: Dict[str, Dict[str, str]] = self.get_readable_tracks_element(cols_to_select)
 
-        with open(_path_to_save, 'w') as file:
+        with open(_path_to_save, "w") as file:
             file.write(json.dumps(readable_tracks_element, indent=4, **kwargs))
 
-    def save_to_yaml(
-            self,
-            path_to_save: Optional[str] = None,
-            cols_to_select: Optional[List[str]] = None,
-            **kwargs
-    ) -> None:
-        _path_to_save: Optional[str] = 'Library.yaml' if not path_to_save else path_to_save
+    def save_to_yaml(self, path_to_save: Optional[str] = None, cols_to_select: Optional[List[str]] = None, **kwargs) -> None:
+        _path_to_save: Optional[str] = "Library.yaml" if not path_to_save else path_to_save
         readable_tracks_element: Dict[str, Dict[str, str]] = self.get_readable_tracks_element(cols_to_select)
 
-        with open(_path_to_save, 'w') as file:
+        with open(_path_to_save, "w") as file:
             yaml.dump(readable_tracks_element, file, default_flow_style=False, allow_unicode=True, **kwargs)
 
-    def _to_dataframe(
-            self,
-            cols_to_select: Optional[List[str]] = None,
-            **kwargs
-    ) -> pd.DataFrame:
+    def _to_dataframe(self, cols_to_select: Optional[List[str]] = None, **kwargs) -> pd.DataFrame:
         readable_tracks_element: Dict[str, Dict[str, str]] = self.get_readable_tracks_element()
         readable_tracks_element_dataframe: pd.DataFrame = pd.DataFrame.from_dict(readable_tracks_element).T
 
@@ -123,14 +107,8 @@ class AppleMusicParseXML:
 
         return readable_tracks_element_dataframe
 
-    def save_to_csv(
-            self,
-            path_to_save: Optional[str] = None,
-            cols_to_select: Optional[List[str]] = None,
-            show_index: bool = False,
-            **kwargs
-    ) -> None:
-        _path_to_save: Optional[str] = 'Library.csv' if not path_to_save else path_to_save
+    def save_to_csv(self, path_to_save: Optional[str] = None, cols_to_select: Optional[List[str]] = None, show_index: bool = False, **kwargs) -> None:
+        _path_to_save: Optional[str] = "Library.csv" if not path_to_save else path_to_save
         readable_tracks_element_dataframe: pd.DataFrame = self._to_dataframe(cols_to_select)
 
         readable_tracks_element_dataframe.to_csv(_path_to_save, index=show_index, **kwargs)
